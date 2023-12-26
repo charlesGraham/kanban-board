c5a07e7781ff329f18b867658da8338e2f977497
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-26 17:23:31 -0600

    Added connection to MongoDB

---
+++ API/app.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer); // for websocket requests
+
+
+app.get('/', (req, res) => {
+    res.send("API is up");
+});
+
+io.on("connection", () => {
+    console.log("Connected!");
+});
+
+mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@localhost:27017/`)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+        httpServer.listen(4001, () => {
+            console.log(`App is listening on port 4001`);
+        });
+    })
+
