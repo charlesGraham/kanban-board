172e9d1fb8d5123d6321f1e360b274dec6bad8bc
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-26 20:43:12 -0600

    Added UsersController + register method. Can create users in MongoDB now

--- API/src/app.ts
+++ API/src/app.ts
@@ -3,15 +3,22 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
+import * as usersController from "./controllers/users";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer); // for websocket requests
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send("API is up");
 });
+
+app.post("/api/users", usersController.register);
 
 io.on("connection", () => {
     console.log("Connected!");
@@ -23,4 +30,4 @@ mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGOD
         httpServer.listen(4001, () => {
             console.log(`App is listening on port 4001`);
         });
-    })
+    });
---
+++ API/src/controllers/users.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import UserModel from "../models/user";
+import { UserDocument } from "../types/user.interface";
+import { Error } from "mongoose";
+
+const normalizeUser = (user: UserDocument) => {
+    return {
+        email: user.email,
+        username: user.username,
+        id: user.id
+    };
+}
+
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const newUser = new UserModel({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        const savedUser = await newUser.save();
+        res.send(normalizeUser(savedUser));
+    } catch (err) {
+        if (err instanceof Error.ValidationError) {
+            const messages = Object.values(err.errors).map(err => err.message);
+            return res.status(422).json(messages);
+        }
+        next(err);
+    }
+}
